d8531b15114347a7971a759e4d1018946569580c
Filip Bengtegård <bengtegardbook@gmail.com>
2024-06-26 17:14:46 +0200

    remove files4

--- src/lib/fetcher-authed.ts
+++
@@ -1,4 +0,0 @@
-// @ts-ignore
-export const authedFetcher = (...args) =>
-  // @ts-ignore
-  fetch(...args, { credentials: "include" }).then((res) => res.json());
--- src/lib/fetcher.ts
+++
@@ -1,4 +0,0 @@
-// @ts-ignore
-export const fetcher = (...args: any) =>
-  // @ts-ignore
-  fetch(...args).then((res) => res.json());
